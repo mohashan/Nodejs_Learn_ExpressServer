4f61943c5e5c1c54ff18ea7d6cf9f840f23e8e13
moha <msh200x@gmail.com>
2018-07-24 12:21:06 +0430

    Add Some Middleware to handle log and errors

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ const app = express();
 // to create a virtual path to serve static files
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.use(function (req, res, next) {
+    console.log('new Request Received to : ' + req.url);
+    req.ReceivedTimer = new Date();
+    next();
+});
+
+
+
+app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
+    next();
+});
+
+app.use((req) => {
+    console.log('Reponse Processed in(ms) : ' + ((new Date()) - (req.ReceivedTimer?req.ReceivedTimer:(new Date()))).toString());
+});
+
+app.use(function (err, req, res, next) {
+    console.log('Reponse Processed in(ms) : ' + ((new Date()) - req.ReceivedTimer).toString());
+
+    console.error(err);
+    res.end('An Error Has Occured');
 });
 
 app.listen(3000, (err) => {
@@ -16,3 +36,4 @@ app.listen(3000, (err) => {
     }
     console.log('Server is created on port 3000');
 });
+
